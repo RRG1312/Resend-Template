905ab99dbe83c1e335c6f48c21f8198a4fa420ce
Ruben <ruben.ben111@gmail.com>
2025-09-02 23:41:05 +0200

    variable de entorno cambiada

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 
-const resend = new Resend('you_wont_see_mmy_api_key_xd');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
   try {
